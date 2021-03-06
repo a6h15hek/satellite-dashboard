79b2b6a47d1ba946113098019d93205480cc949c
a6h15hek <a6h15hek@outlook.com>
2021-03-06 17:03:07 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,34 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import './globalstyle.css';
 import * as serviceWorker from './serviceWorker';
-import { createBrowserHistory } from "history";
-import { Router, Route, Switch } from "react-router-dom";
+import App from './App';
 
-
-// pages for this product
-import HomePage from "views/HomePage/HomePage";
-import AboutUsPage from "views/AboutUsPage/AboutUsPage";
-import LoginPage from "views/LoginPage/LoginPage";
-import PageNotFound from "views/PageNotFound/PageNotFound";
-import SignUpPage from "views/SignUpPage/SignUpPage";
-import Dashboard from "views/Dashboard/Dashboard";
-
-//testing .env file
-//console.log(process.env);
-
-var hist = createBrowserHistory();
 ReactDOM.render(
-  <Router history={hist}>
-    <Switch>
-      <Route exact path="/" component={Dashboard} />
-      <Route path="/aboutus" component={AboutUsPage} />
-      <Route path="/signup" component={SignUpPage} />
-      <Route path="/login" component={LoginPage} />
-      <Route path="/homepage" component={HomePage} />
-      <Route component={PageNotFound} />
-    </Switch>
-  </Router>,
+  <BrowserRouter>
+      <App />
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
